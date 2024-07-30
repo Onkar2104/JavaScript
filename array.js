9196a6aa3b5e976dbecadbdb0097f8529b2e4fc5
Onkar Ijare <87595561+Onkar2104@users.noreply.github.com>
2024-07-30 16:33:00 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+let student = ["aman", "shraddha", "onkar"];
+console.log(student);
+console.log(student[1]);    //shraddha
+console.log(student[0]="pratik");
+console.log(student);
+
+let info = ["onkar", 20, 99.9];
+console.log(info);
+
+let em = [];
+console.log(em);    //empty array
